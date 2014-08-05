b1bc762b50601e8114d51666dab1ae01bbbc476f
James Dowell <jamesdowell1@gmail.com>
2014-08-05 21:30:35 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name financeApp
+ * @description
+ * # financeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('financeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name financeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the financeApp
+ */
+angular.module('financeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
